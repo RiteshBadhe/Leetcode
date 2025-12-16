0298a8335252f1715a83a5bc57b228ced366dd51
RiteshBadhe <riteshbadhe737@gmail.com>
2025-12-16 16:48:44 +0530

    Time: 1 ms (99.94%) | Memory: 43.4 MB (66.73%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Solution {
+
+    public static int myAtoi(String s) {
+        s = s.trim();
+        if (s.isEmpty()) return 0;
+
+        int sign = 1, i = 0;
+        long result = 0;
+
+     
+        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
+            sign = s.charAt(0) == '-' ? -1 : 1;
+            i++;
+        }
+
+        
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+
+        
+            if (result * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (result * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+            i++;
+        }
+
+        return (int) (result * sign);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number as string: ");
+        String str = sc.nextLine();
+
+        int num = myAtoi(str);
+        System.out.println("Converted integer: " + num);
+    }
+}
