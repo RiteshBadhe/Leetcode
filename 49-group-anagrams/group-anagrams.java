57f6651fb0310f99f9e90b509adea6fdbc099044
RiteshBadhe <riteshbadhe737@gmail.com>
2026-01-06 16:32:01 +0530

    Time: 21 ms (12.85%) | Memory: 50.5 MB (16.84%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+
+            int[] freq = new int[26];
+
+            for (char c : s.toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+       
+            StringBuilder keyBuilder = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                keyBuilder.append('#');
+                keyBuilder.append(freq[i]);
+            }
+
+            String key = keyBuilder.toString();
+
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
